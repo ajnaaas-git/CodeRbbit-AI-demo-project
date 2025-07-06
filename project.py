5fc07bfa578c498635ad88ff7389a8ea5d011c05
ajnaaas-git <muhammedajnas0303@gmail.com>
2025-07-06 16:43:36 +0530

    Create project.py

---
+++ project.py
@@ -0,0 +1,13 @@
+
+def calculate_sum(a, b) 
+    result = a + b
+    print("The sum is:", result)
+print(x) 
+num_str = "10"
+num_int = 5
+sum_mixed = num_str + num_int 
+my_list = [1, 2, 3]
+print(my_list[3]) 
+numerator = 10
+denominator = 0
+division_result = numerator / denominator 
